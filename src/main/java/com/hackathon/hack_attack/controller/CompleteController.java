d487e02eb292963780cbb0d9705cea19ed96976e
1192728 <vasuryavinu@gmail.com>
2024-07-21 10:01:20 +0530

    api

--- src/main/java/com/hackathon/hack_attack/controller/CompleteController.java
+++ src/main/java/com/hackathon/hack_attack/controller/CompleteController.java
@@ -2,10 +2,7 @@ package com.hackathon.hack_attack.controller;
 
 import com.hackathon.hack_attack.service.CompleteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping
 @RestController
@@ -14,6 +11,7 @@ public class CompleteController {
     private CompleteService completeService;
 
     @GetMapping
+    @CrossOrigin(origins = "*")
     private Object returnAccount(@RequestBody String accountId) {
         return completeService.fetch(accountId);
     }
